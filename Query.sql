744916bfa0922397b6f2cc10dc0ab04968a62e8e
soumendra978 <105708323+soumendra978@users.noreply.github.com>
2022-05-19 12:53:31 +0800

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -1,11 +1,11 @@
 -- I want to know the list of our customers and their spending.
-select cid, cname, sum(prodcost) from dealer_table
+select cid, cname, sum(prodcost) from dealer
 group by cid, cname;
 
 
 -- I want to find out the top 3 car manufacturers that customers bought by sales (quantity) and the sales number -- for it in the current month.
 select * from (select prodmanuf, count(prodmanuf), rank() over(order by count(prodmanuf) desc) rn
-from table_name
+from dealer
 where trunc(sale_date,'MM') = trunc(sysdate,'MM')
-group by deptid)
+group by prodmanuf)
 where rn < 4
